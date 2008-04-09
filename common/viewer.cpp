a7dde2f656f2ff8d6c8be5f4e3c5215c5bd6c03f
Daniel Fiser <danfis@danfis.cz>
2008-04-09 19:33:13 +0200

    Implemented lights in Viewer class.

--- common/viewer.cpp
+++ common/viewer.cpp
@@ -3,11 +3,28 @@
 #include "viewer.hpp"
 using namespace std;
 
+void GSRM::ViewerCameraChangedCallback(void *data, SoSensor *)
+{
+    GSRM::Viewer *viewer = (GSRM::Viewer *)data;
+    viewer->_setUpLightPosition();
+}
+
 GSRM::Viewer::Viewer(QWidget *parent, const char *name)
     : SoQtExaminerViewer(parent, name)
 {
+    _light = new SoPointLight;
+    _light->ref();
+    _root = new SoSeparator;
+    _root->ref();
+
+    _light_transform.setValue(0, 0, 0);
 }
 
+GSRM::Viewer::~Viewer()
+{
+    _light->unref();
+    _root->unref();
+}
 
 void GSRM::Viewer::addToggleButton(ViewerPushButtonCallback callback,
                                    void *closure)
@@ -50,4 +67,92 @@ void GSRM::Viewer::show()
     }
 
     SoQtExaminerViewer::show();
+
+    _initLight();
+    _initCallbacks();
+}
+
+void GSRM::Viewer::setSceneGraph(SoNode *root)
+{
+    _root->removeAllChildren();
+    _root->addChild(_light);
+    _root->addChild(root);
+
+    SoQtExaminerViewer::setSceneGraph(_root);
+    _initLight();
+}
+
+
+// private:
+void GSRM::Viewer::_initLight()
+{
+    _light->on = true;
+    _light->intensity = 1;
+    _light->color.setValue(1,1,1);
+
+    _setUpLightPosition();
+}
+
+void GSRM::Viewer::_initCallbacks()
+{
+    SoCamera *camera = this->getCamera();
+    if (camera == NULL){
+        DBG("No camera");
+        return;
+    }
+    SoFieldSensor *cam_sensor =
+        new SoFieldSensor(ViewerCameraChangedCallback, this);
+    cam_sensor->attach(&camera->position);
+}
+
+void GSRM::Viewer::_setUpLightPosition()
+{
+    SoCamera *cam = this->getCamera();
+    SbVec3f pos;
+    float x, y, z;
+
+    if (cam != NULL){
+        pos = cam->position.getValue();
+        x = pos[0];
+        y = pos[1];
+        z = pos[2];
+
+        x = pos[0]*cos(_light_transform[0]) - pos[2]*sin(_light_transform[0]);
+        y = pos[1]*cos(_light_transform[1]) - pos[2]*sin(_light_transform[1]);
+        z = pos[1]*cos(_light_transform[0]) + pos[2]*sin(_light_transform[0]) +
+            pos[1]*cos(_light_transform[1]) + pos[2]*sin(_light_transform[1]);
+
+        x = (1 + _light_transform[2]) * x;
+        y = (1 + _light_transform[2]) * y;
+        z = (1 + _light_transform[2]) * z;
+
+        pos.setValue(x, y, z);
+
+        lock();
+        _light->location.setValue(pos * -100);
+        unlock();
+    }else{
+        SoDB::writelock();
+        _light->location.setValue(0,0,0);
+        SoDB::writeunlock();
+    }
+}
+
+
+void GSRM::Viewer::leftWheelMotion(float val)
+{
+    _light_transform[0] = val;
+    _setUpLightPosition();
+}
+
+void GSRM::Viewer::bottomWheelMotion(float val)
+{
+    _light_transform[1] = val;
+    _setUpLightPosition();
+}
+
+void GSRM::Viewer::rightWheelMotion(float val)
+{
+    _light_transform[2] = val;
+    _setUpLightPosition();
 }
--- common/viewer.hpp
+++ common/viewer.hpp
@@ -3,11 +3,18 @@
 
 #include <vector>
 #include <Inventor/Qt/viewers/SoQtExaminerViewer.h>
+#include <Inventor/nodes/SoSeparator.h>
+#include <Inventor/nodes/SoPointLight.h>
+#include <Inventor/nodes/SoCamera.h>
+#include <Inventor/sensors/SoFieldSensor.h>
 #include <Inventor/threads/SbMutex.h>
 
+#include "msg.hpp"
 #include "viewer_push_button.hpp"
 
 namespace GSRM {
+
+void ViewerCameraChangedCallback(void *data, SoSensor *);
 
 /**
  * Viewer based on SoQtExaminerViewer
@@ -17,12 +24,24 @@ class Viewer : public QObject, public SoQtExaminerViewer{
     SbMutex _lock; /*! internal lock for multithreading */
     std::vector<ViewerPushButton *> _buttons; /*! list of registered buttons */
 
+    SoPointLight *_light;
+    SoSeparator *_root;
+    SbVec3f _light_transform;
+
   protected:
     virtual SbBool processSoEvent(const SoEvent *const event);
     virtual void actualRedraw(void);
 
+    virtual void _initCallbacks();
+    virtual void _initLight();
+    virtual void _setUpLightPosition();
+
+    void leftWheelMotion(float val);
+    void bottomWheelMotion(float val);
+    void rightWheelMotion(float val);
   public:
     Viewer(QWidget *parent, const char *name = "");
+    virtual ~Viewer();
 
     /**
      * Lock/Unlock viewer
@@ -37,6 +56,9 @@ class Viewer : public QObject, public SoQtExaminerViewer{
                          void *closure = NULL);
 
     virtual void show();
+    void setSceneGraph(SoNode *root);
+
+    friend void ViewerCameraChangedCallback(void *data, SoSensor *);
 };
 
 }
