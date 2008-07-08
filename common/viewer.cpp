6b4f448b2e2bd5ad7cb6ac15d3d24e70f828e115
Daniel Fiser <danfis@danfis.cz>
2008-07-08 19:02:23 +0200

    Added notices about license.

--- common/viewer.cpp
+++ common/viewer.cpp
@@ -1,3 +1,25 @@
+/**
+ * Coin3dTools
+ * ------------
+ * Copyright (c)2007 Daniel Fiser <danfis (at) danfis (dot) cz>
+ *
+ *
+ * This file is part of Coin3dTools
+ *
+ * Coin3dTools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Coin3dTools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Coin3dTools.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <iostream>
 #include <algorithm>
 #include <QVBoxLayout>
