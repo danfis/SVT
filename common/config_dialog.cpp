61ffe5bac19bbb9b52422c5ac6cb2c7ffccc066d
Daniel Fiser <danfis@danfis.cz>
2008-08-22 19:29:43 +0200

    Changed name of this project from Coin3dTools to SVT!

--- common/config_dialog.cpp
+++ common/config_dialog.cpp
@@ -1,23 +1,23 @@
 /**
- * Coin3dTools
- * ------------
- * Copyright (c)2007 Daniel Fiser <danfis (at) danfis (dot) cz>
+ * SVT
+ * ----------
+ * Copyright (c)2007,2008 Daniel Fiser <danfis (at) danfis (dot) cz>
  *
  *
- * This file is part of Coin3dTools
+ * This file is part of SVT
  *
- * Coin3dTools is free software: you can redistribute it and/or modify
+ * SVT is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Coin3dTools is distributed in the hope that it will be useful,
+ * SVT is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Coin3dTools.  If not, see <http://www.gnu.org/licenses/>.
+ * along with SVT. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <list>
